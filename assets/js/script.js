26a9953239cbee02614cf8bb717ad9c48a6b5bc6
Mugisha-Beline <belinemugisha21@gmail.com>
2023-07-18 15:12:33 +0200

    updated search form

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,47 @@ function showSignupForm() {
 function showAlert() {
   alert("you are successfully logged in");
 }
+
+
+const animals = [
+  { name: "African Elephant", description: "The African Elephant is the largest land mammal, characterized by its long trunk, large ears, and tusks." },
+  { name: "Bengal Tiger", description: "The Bengal Tiger is a powerful carnivorous cat native to the Indian subcontinent. It has a distinctive orange coat with black stripes and is known for its strength and agility." },
+  // Add more animals here
+];
+
+// Function to perform the search
+function searchAnimals() {
+  const query = document.getElementById("searchInput").value.toLowerCase();
+  const searchResults = animals.filter(animal => {
+    const name = animal.name.toLowerCase();
+    const description = animal.description.toLowerCase();
+    return name.includes(query) || description.includes(query);
+  });
+
+  displayResults(searchResults);
+}
+
+// Function to display the search results
+function displayResults(results) {
+  const resultContainer = document.getElementById("result");
+  resultContainer.innerHTML = ""; // Clear previous results
+
+  if (results.length === 0) {
+    resultContainer.innerText = "No matching animals found.";
+  } else {
+    results.forEach(animal => {
+      const resultElement = document.createElement("div");
+      resultElement.classList.add("search-result");
+      resultElement.innerHTML = `<h3>${animal.name}</h3><p>${animal.description}</p>`;
+      resultContainer.appendChild(resultElement);
+    });
+  }
+}
+const searchForm = document.getElementById("search-form");
+
+searchForm.addEventListener("submit", function(e){
+  e.preventDefault();
+  searchAnimals();
+
+})
+
